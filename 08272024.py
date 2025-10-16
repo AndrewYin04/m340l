25aecc430ce0f6c198113ee6b1c126da79b4b9dc
AndrewYin04 <andrewtcyin@gmail.com>
2025-10-15 20:36:21 -0700

    more

---
+++ 08272024.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy import linalg
+from sympy import *
+import matplotlib.pyplot as plt
+# %matplotlib inline
+init_printing(pretty_print=True, use_latex=True)
+from scipy.linalg import norm
+breakpoint()
+
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
